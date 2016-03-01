50400cf41e9f6ea941313dc65c83f918c018bae2
Alex Fallenstedt and Ryan Brown <a.fallenstedt@gmail.com>
2016-03-01 11:16:34 -0800

    add passing set and get enroll date specs and functions

--- tests/StudentTest.php
+++ tests/StudentTest.php
@@ -65,6 +65,22 @@
 			//Assert
 			$this->assertEquals(1, $result);
 		}
+
+		function setEnrollDate()
+		{
+			//Arrange
+			$name = "Goofus";
+			$enroll_date = '2000-12-30';
+			$id = null;
+			$test_student = new Student($id, $name, $enroll_date);
+
+			//Act
+			$test_student->setEnrollDate('2020-12-30');
+			$result = $test_student->getEnrollDate();
+
+			//Assert
+			$this->assertEquals('2020-12-30', $result);
+		}
 	}
 
 
