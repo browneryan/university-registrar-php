348269561db6ab530f5a0bb582f892037032af9b
Alex Fallenstedt and Ryan Brown <browneryan87@gmail.com>
2016-03-01 08:50:12 -0800

    Add initial project files

---
+++ src/Course.php
@@ -0,0 +1,14 @@
+<?php
+	 class Course
+		{
+		// private property;
+		//
+		//  function getProperty(){
+		// 	return $this->property;
+		// 	}
+		//
+		// function setProperty(){
+		// 	$this->property = $property;
+		// }
+	}
+ ?>
---
+++ src/Student.php
@@ -0,0 +1,14 @@
+<?php
+	 class Student
+		{
+		// private property;
+		//
+		//  function getProperty(){
+		// 	return $this->property;
+		// 	}
+		//
+		// function setProperty(){
+		// 	$this->property = $property;
+		// }
+	}
+ ?>
